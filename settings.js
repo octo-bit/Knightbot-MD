2b30a798c7728b3f9589ab5248fa805e096d9482
octo-bit <octo43072@gmail.com>
2025-05-31 15:47:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'CORNELIUS', // Your name
-  ownerNumber: '254797348876', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'cornelius', // Your name
+  ownerNumber: '2540797348876', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
